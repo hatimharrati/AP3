22f2514b9f90380e4159fb458ef4b8d94c7f273c
Hatim Harrati <hatimharratipro@gmail.com>
2024-12-18 10:18:35 +0100

    Delete AP3 directory

--- AP3/billing-service/src/main/java/org/sid/billingservice/repository/BillRepository.java
+++
@@ -1,7 +0,0 @@
-package org.sid.billingservice.repository;
-
-import org.sid.billingservice.entities.Bill;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BillRepository extends JpaRepository<Bill, Long> {
-}
